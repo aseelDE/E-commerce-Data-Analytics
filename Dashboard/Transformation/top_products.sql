e1c6ed49fbf96b360fb92a8453f2ea6950d42cad
aseelDE <alzahraniaseel0@gmail.com>
2026-04-29 10:30:59 +0300

    Delete Dashboard/Transformation directory

--- Dashboard/Transformation/top_products.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TABLE `e-commerce-de-project.ecommerce_analytics.top_products` AS
-(
-    SELECT id, title, category, price, rating.rate, rating.count FROM `ecommerce_raw.ecommercerawproducts`
-ORDER BY rating.rate DESC
-)
-
-
